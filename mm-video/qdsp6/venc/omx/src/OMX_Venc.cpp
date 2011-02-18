4e93a80262c924649ccf95014eecae9e76cc7c5f
preludedrew <andrew.boren@gmail.com>
2011-02-17 22:43:29 -0700

    update from CM

--- mm-video/qdsp6/venc/omx/src/OMX_Venc.cpp
+++ mm-video/qdsp6/venc/omx/src/OMX_Venc.cpp
@@ -85,8 +85,6 @@ static const char pRoleAVC[] = "video_encoder.avc";
 /*----------------------------------------------------------------------------
 * Static Variable Definitions
 * -------------------------------------------------------------------------*/
-Venc* Venc::g_pVencInstance = NULL;
-
 #define VEN_QCIF_DX                 176
 #define VEN_QCIF_DY                 144
 
@@ -124,7 +122,7 @@ Venc* Venc::g_pVencInstance = NULL;
 extern "C" {
    void* get_omx_component_factory_fn(void)
    {
-     return Venc::get_instance();
+     return new Venc();
    }
 }
 #define GetLastError() 1
@@ -170,7 +168,6 @@ Venc::Venc() :
 
 Venc::~Venc()
 {
-  g_pVencInstance = NULL;
   QC_OMX_MSG_HIGH("deconstructor (closing driver)");
   sem_destroy(&m_cmd_lock);
   ven_device_close(m_pDevice);
@@ -575,18 +572,6 @@ bail:
     free(m_pComponentName);
   }
   return result;
-}
-
-Venc* Venc::get_instance()
-{
-  QC_OMX_MSG_HIGH("getting instance...");
-  if (g_pVencInstance)
-  {
-    QC_OMX_MSG_ERROR("Singleton Class can't created more than one instance");
-    return NULL;
-  }
-  g_pVencInstance = new Venc();
-  return g_pVencInstance;
 }
 
 OMX_ERRORTYPE Venc::get_component_version(OMX_IN  OMX_HANDLETYPE hComponent,
@@ -1655,8 +1640,13 @@ OMX_ERRORTYPE Venc::update_param_port_fmt(OMX_IN OMX_VIDEO_PARAM_PORTFORMATTYPE*
   return result;
 }
 
-OMX_ERRORTYPE Venc::update_param_port_def(OMX_IN OMX_PARAM_PORTDEFINITIONTYPE* pParam)
+OMX_ERRORTYPE Venc::update_param_port_def(OMX_IN OMX_PARAM_PORTDEFINITIONTYPE* pOrigParam)
 {
+  // The code below tries to write to pParam - keep a local
+  // version instead of writing into the user specified data
+  // (which might be in read-only memory in some cases), and
+  // point pParam to that.
+  OMX_PARAM_PORTDEFINITIONTYPE localParam = *pOrigParam, *pParam = &localParam;
   OMX_ERRORTYPE result = OMX_ErrorNone;
   int rc = 0;
   if (pParam != NULL)
