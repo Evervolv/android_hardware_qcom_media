16d4a35df19957a9406a964400f1a676d2e956e0
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-03-13 00:45:49 -0500

    qcom media: remove old omx libs
    Change-Id: Icb6a1e15d78344ff2c5ae1704722744d136b3358

--- Android.mk
+++ Android.mk
@@ -1,11 +1,4 @@
-QCOM_MEDIA_ROOT := $(call my-dir)
-
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-	media-hals := libstagefrighthw
+	media-hals := libstagefrighthw audio
 	include $(call all-named-subdir-makefiles,$(media-hals))
-	include $(QCOM_MEDIA_ROOT)/audio/Android.mk
-ifeq ($(BOARD_USES_QCOM_LIBS),true)
-	include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
-	include $(QCOM_MEDIA_ROOT)/mm-video/Android.mk
-endif
 endif
