95353970dafc3a6da20d94a6f8407f0afd1e444b
andrew.boren <andrew.boren@gmail.com>
2013-01-11 22:26:58 -0700

    qcom/media: Limit to 8960 devices for the time being.
    Change-Id: I8b1b893e55f54aae1260e0746f0e1c7baad53526

--- Android.mk
+++ Android.mk
@@ -4,9 +4,3 @@ ifneq ($(filter msm8960,$(TARGET_BOARD_PLATFORM)),)
 include $(call all-subdir-makefiles)
 
 endif
-
-ifneq ($(filter msm8660,$(TARGET_BOARD_PLATFORM)),)
-
-include $(LOCAL_PATH)/mm-core/Android.mk
-
-endif
