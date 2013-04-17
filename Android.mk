7ff369d7b74c744bdee162659af095b590439ab9
Nick Reuter <nreuter85@gmail.com>
2013-04-17 10:56:59 -0500

    media: let any qcom device use this repo
    -filter out if using display legacy
    
    Change-Id: I830052143151b8521eef72464bb9cda8bc015c73

--- Android.mk
+++ Android.mk
@@ -1,5 +1,7 @@
-ifneq ($(filter msm8960,$(TARGET_BOARD_PLATFORM)),)
+ifeq ($(call is-vendor-board-platform,QCOM),true)
+ifneq ($(TARGET_QCOM_DISPLAY_VARIANT),legacy)
 
 include $(call all-subdir-makefiles)
 
+endif
 endif
